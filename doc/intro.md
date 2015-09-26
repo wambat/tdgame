9b52f9eefa26e2381b6a34c9230f7114a4678df7
Mike Konkov <noospheratum@gmail.com>
2015-09-27 00:29:29 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tdgame
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject com.chiefcoder/tdgame "0.1.0-SNAPSHOT"
+  :description "Study for game"
+  :url ""
+  :license {:name ""
+            :url ""}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.chiefcoder/threedom "0.1.0-SNAPSHOT"]
+                 [org.clojars.ndepalma/jme-game-engine "3.0"]])
